9118bd1e0ecced5fefa8489af5b37b11de8a63fe
Moritz Onken <moritz.onken@merckgroup.com>
2018-11-12 12:54:04 +0100

    eslint

--- scripts/testsuite.js
+++ scripts/testsuite.js
@@ -1,80 +1,77 @@
-const path = require("path");
-const fs = require("fs");
+const path = require('path');
+const fs = require('fs');
 
-var CREDENTIALS = {
-  accessKeyId: "AKIDEXAMPLE",
-  secretAccessKey: "wJalrXUtnFEMI/K7MDENG+bPxRfiCYEXAMPLEKEY"
-};
-var SERVICE = "service";
+const SERVICE = 'service';
 
-var suiteDir = path.join(__dirname, "../test/aws-sig-v4-test-suite");
-var ignoreDirs = ["get-header-value-multiline"]; // too annoying to parse multiline
-var tests = fs.readdirSync(suiteDir);
+const suiteDir = path.join(__dirname, '../test/aws-sig-v4-test-suite');
+const ignoreDirs = ['get-header-value-multiline']; // too annoying to parse multiline
+let tests = fs.readdirSync(suiteDir);
 tests = tests
   .concat(
-    ...tests.map(file => {
-      if (file.indexOf(".") > -1) return null; // if file
+    ...tests.map((file) => {
+      if (file.indexOf('.') > -1) return null; // if file
       return fs.readdirSync(path.join(suiteDir, file)).map(d => path.join(file, d));
-    })
+    }),
   )
-  .filter(function(t) {
-    return !~t.indexOf(".") && !~ignoreDirs.indexOf(t);
-  });
+  .filter(t => t.indexOf('.') === -1 && ignoreDirs.indexOf(t) === -1);
 
-const job = tests.sort().map(function(test) {
-  var files = fs.readdirSync(path.join(suiteDir, test));
-  if(!files.find(f => f.indexOf('.req') !== -1)) return;
-  var readFile = function(regex) {
-    var file = path.join(suiteDir, test, files.filter(regex.test.bind(regex))[0]);
-    return fs.readFileSync(file, "utf8").replace(/\r/g, "");
-  };
-  var request = readFile(/\.req$/);
-  var canonicalString = readFile(/\.creq$/);
-  var stringToSign = readFile(/\.sts$/);
-  var outputAuth = readFile(/\.authz$/);
+const job = tests
+  .sort()
+  .map((test) => {
+    const files = fs.readdirSync(path.join(suiteDir, test));
+    if (!files.find(f => f.indexOf('.req') !== -1)) return;
+    const readFile = (regex) => {
+      const file = path.join(suiteDir, test, files.filter(regex.test.bind(regex))[0]);
+      return fs.readFileSync(file, 'utf8').replace(/\r/g, '');
+    };
+    const request = readFile(/\.req$/);
+    const canonicalString = readFile(/\.creq$/);
+    const stringToSign = readFile(/\.sts$/);
+    const outputAuth = readFile(/\.authz$/);
 
-  var reqLines = request.split("\n");
-  var req = reqLines[0].split(" ");
-  var method = req[0];
-  var pathname = req.slice(1, -1).join(" ");
-  var headers = {};
-  for (var i = 1; i < reqLines.length; i++) {
-    if (!reqLines[i]) break;
-    var colonIx = reqLines[i].indexOf(":");
-    var header = reqLines[i].slice(0, colonIx).toLowerCase();
-    var value = reqLines[i].slice(colonIx + 1);
-    if (headers[header]) {
-      headers[header] = headers[header].split(",");
-      headers[header].push(value);
-      headers[header] = headers[header].join(",");
-    } else {
-      headers[header] = value;
+    const reqLines = request.split('\n');
+    const req = reqLines[0].split(' ');
+    const method = req[0];
+    const pathname = req.slice(1, -1).join(' ');
+    const headers = {};
+    let i;
+    for (i = 1; i < reqLines.length; i++) {
+      if (!reqLines[i]) break;
+      const colonIx = reqLines[i].indexOf(':');
+      const header = reqLines[i].slice(0, colonIx).toLowerCase();
+      const value = reqLines[i].slice(colonIx + 1);
+      if (headers[header]) {
+        headers[header] = headers[header].split(',');
+        headers[header].push(value);
+        headers[header] = headers[header].join(',');
+      } else {
+        headers[header] = value;
+      }
     }
-  }
-  var body = reqLines.slice(i + 1).join("\n");
+    const body = reqLines.slice(i + 1).join('\n');
 
-  var signer = {
-    service: SERVICE,
-    method: method,
-    path: pathname,
-    headers: headers,
-    body: body,
-    doNotModifyHeaders: true,
-    doNotEncodePath: true
-  };
+    const signer = {
+      service: SERVICE,
+      method,
+      path: pathname,
+      headers,
+      body,
+      doNotModifyHeaders: true,
+      doNotEncodePath: true,
+    };
 
-  return {
-    test,
-    request: signer,
-    canonicalString,
-    stringToSign,
-    outputAuth
-  };
+    return {
+      test,
+      request: signer,
+      canonicalString,
+      stringToSign,
+      outputAuth,
+    };
+  })
+  .filter(f => f);
 
-  // signer.canonicalString().should.equal(canonicalString);
-  // signer.stringToSign().should.equal(stringToSign);
-  // signer.sign().headers.Authorization.should.equal(outputAuth);
-}).filter(f => f);
-
-fs.writeFileSync(path.join(__dirname, "../test/aws-sig-v4-test-suite.json"), JSON.stringify(job, null, 2));
+fs.writeFileSync(
+  path.join(__dirname, '../test/aws-sig-v4-test-suite.json'),
+  JSON.stringify(job, null, 2),
+);
 console.log(`${job.length} tests exported`);
