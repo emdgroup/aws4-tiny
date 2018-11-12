7a0f52ed49f716bbd4db369976ac780dee028a95
Moritz Onken <moritz.onken@merckgroup.com>
2018-11-12 14:10:43 +0100

    eslinted

--- src/buffer.js
+++ src/buffer.js
@@ -1,4 +1,4 @@
-import sjcl from "./sjcl";
+import sjcl from './sjcl';
 
 export default class Buffer {
   static byteLength(str) {
--- src/index.js
+++ src/index.js
@@ -3,28 +3,42 @@ import aws4, { RequestSigner } from 'aws4';
 import urllib from 'url';
 
 aws4.fetch = (...args) => {
-  if(!'fetch' in window) throw 'fetch is not defined';
+  if (!('fetch' in window)) throw new Error('fetch is not defined');
   let signer;
-  if(typeof args[0] === 'string') {
-    const { protocol, host, port, pathname, search } = urllib.parse(args[0]);
-    console.log(protocol);
-    if(args.length == 2) signer = new RequestSigner({
-      host, port, path: `${pathname}${search || ''}`, protocol,
-    }, args[1]);
-    else {
-      const [ , opts, credentials ] = args;
-      signer = new RequestSigner({
-        host, port, path: `${pathname}${search || ''}`, protocol,
-        ...opts,
-      }, credentials);
+  if (typeof args[0] === 'string') {
+    const {
+      protocol, host, port, pathname, search,
+    } = urllib.parse(args[0]);
+    if (args.length === 2) {
+      signer = new RequestSigner(
+        {
+          host,
+          port,
+          path: `${pathname}${search || ''}`,
+          protocol,
+        },
+        args[1],
+      );
+    } else {
+      const [, opts, credentials] = args;
+      signer = new RequestSigner(
+        {
+          host,
+          port,
+          path: `${pathname}${search || ''}`,
+          protocol,
+          ...opts,
+        },
+        credentials,
+      );
     }
-
   } else {
     signer = new RequestSigner(...args);
   }
   const { pathname, search } = urllib.parse(signer.request.path);
   const url = urllib.format({
-    pathname, search,
+    pathname,
+    search,
     ...signer.request,
   });
   signer.sign();
